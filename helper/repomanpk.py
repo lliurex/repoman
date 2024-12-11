156bd3505a9a3f86eb020e9a9bb730d7e74cd54b
lliurex <juanma1980@gmail.com>
2024-12-11 09:33:08 +0100

    Add update and edit

--- helper/repomanpk.py
+++ helper/repomanpk.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-import sys
+import sys,os
 from repoman import repomanager
 import subprocess
 
 class err:
 	SIGNED=10
 
+ret=0
 repo=repomanager.manager()
 if len(sys.argv)>=3:
 	name=sys.argv[1]
@@ -46,4 +47,18 @@ if len(sys.argv)>=3:
 		ret=repo.disableAll()
 	elif state=="enableDefault":
 		ret=repo.enableDefault()
+	elif state=="update":
+		os.system("/usr/bin/apt update")
+	elif state=="edit":
+		editor=os.environ.get("EDITOR","")
+		if editor=="":
+			editors=["vim","nano","mcedit","emacs"]
+			for ed in editors:
+				if os.path.isfile(os.path.join("/","usr","bin",ed)):
+					editor=os.path.join("/","usr","bin",ed)
+					break
+		if os.path.isfile(editor)==True:
+			subprocess.run([editor,name])
+				
+				
 sys.exit(ret)
